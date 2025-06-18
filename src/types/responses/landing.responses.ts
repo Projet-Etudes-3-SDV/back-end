6189bf486041c54fb0026b36a094c548505c82b8
Elie Lajoinie <elielajoinie24@gmail.com>
2025-06-18 21:02:49 +0200

    fix: landing presenter

--- src/types/responses/landing.responses.ts
+++ src/types/responses/landing.responses.ts
@@ -4,6 +4,65 @@ import "reflect-metadata";
 import { HeaderDTO } from "../requests/landing.requests"
 import { ProductPresenter } from "./product.responses";
 import { CategoryPresenter } from "./category.responses";
+import { AlertType } from "../../models/landing.model";
+
+export class LandingPresenter {
+  @Expose()
+  id!: string;
+
+  @Expose()
+  @ValidateNested()
+  @Type(() => HeaderDTO)
+  header!: HeaderDTO;
+
+  @Expose()
+  @ValidateNested()
+  @Type(() => CarouselSectionPresenter)
+  carouselSection?: CarouselSectionPresenter;
+
+  @Expose()
+  @ValidateNested()
+  @Type(() => CategorySectionPresenter)
+  categorySection?: CategorySectionPresenter;
+
+  @Expose()
+  @ValidateNested()
+  @Type(() => AlertSectionPresenter)
+  alert?: AlertSectionPresenter;
+
+  @Expose()
+  isMain!: boolean;
+}
+
+class AlertSectionPresenter {
+  @Expose()
+  title!: string;
+
+  @Expose()
+  description?: string;
+
+  @Expose()
+  type!: AlertType; 
+
+  @Expose()
+  order!: number;
+}
+
+class CarouselSectionPresenter {
+  @Expose()
+  title!: string;
+
+  @Expose()
+  description?: string;
+
+  @Expose()
+  @ValidateNested({ each: true })
+  @Type(() => ProductsPresenter)
+  products!: ProductsPresenter[];
+
+  @Expose()
+  order!: number;
+}
 
 export class CategorySectionPresenter {
   @IsString()
@@ -25,42 +84,6 @@ export class CategorySectionPresenter {
   categories!: CategoryPresenter[];
 }
 
-export class LandingPresenter {
-  @Expose()
-  id!: string;
-
-  @Expose()
-  @ValidateNested()
-  @Type(() => HeaderDTO)
-  header!: HeaderDTO;
-
-  @Expose()
-  @ValidateNested()
-  @Type(() => CarouselSectionPresenter)
-  carouselSection?: CarouselSectionPresenter;
-
-  @Expose()
-  @ValidateNested()
-  @Type(() => CategorySectionPresenter)
-  categorySection?: CategorySectionPresenter;
-}
-
-class CarouselSectionPresenter {
-  @Expose()
-  title!: string;
-
-  @Expose()
-  description?: string;
-
-  @Expose()
-  @ValidateNested({ each: true })
-  @Type(() => ProductsPresenter)
-  products!: ProductsPresenter[];
-
-  @Expose()
-  order!: number;
-}
-
 class ProductsPresenter {
   @Expose()
   @Type(() => ProductPresenter)
@@ -69,3 +92,4 @@ class ProductsPresenter {
   @Expose()
   order!: number;
 }
+
